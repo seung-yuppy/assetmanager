8b8d6c1d94437f7df555d2e19cc8b7821dd70c04
seung-yuppy <songsy0107@gmail.com>
2025-11-19 15:01:57 +0900

    Fix: 채팅방 아이콘 위치 변경

--- src/main/java/edu/example/assetmanager/controller/AdminItemController.java
+++ src/main/java/edu/example/assetmanager/controller/AdminItemController.java
@@ -1,10 +1,15 @@
 package edu.example.assetmanager.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import edu.example.assetmanager.domain.ItemDTO;
